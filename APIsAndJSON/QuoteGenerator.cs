407205dee27d9dfb1b8880d3512b63b0b950c245
Bex <bexmetzger@gmail.com>
2023-10-17 08:13:15 -0700

    QuoteGenerator

---
+++ APIsAndJSON/QuoteGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
+{
+	public class QuoteGenerator
+	{
+        private HttpClient _client;
+
+        public QuoteGenerator(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string KanyeQuote()
+        {
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonSwansonQuote()
+        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronQuote;
+        }
+    }
+}
+
